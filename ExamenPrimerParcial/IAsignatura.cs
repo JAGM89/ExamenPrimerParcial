31a575cbca6642a98e545d7373c8ca83afe50cd8
JAGM89 <joel.ag2004@gmail.com>
2024-11-07 15:12:17 -0600

    creacion de los parametros en la interfaz

--- ExamenPrimerParcial/IAsignatura.cs
+++ ExamenPrimerParcial/IAsignatura.cs
@@ -3,5 +3,16 @@ namespace ExamenPrimerParcial
 {
     public interface IAsignatura
     {
+        public double CalcularNotaFinal();//metodo sin parametros
+        public double CalcularNotaFinal(double N1, double N2, double N3); //metodo con parametros
+        public string MensajeNotaFinal (double notaFinal);//Mensaje
+
+        public void Imprimir();
+
+
+
+
+
+
     }
 }
