7b566e310e7dbe630bd1deb57c3540e97e1a604c
JAGM89 <joel.ag2004@gmail.com>
2024-11-06 21:27:48 -0600

    cambios en variables

--- ExamenPrimerParcial/Alumno.cs
+++ ExamenPrimerParcial/Alumno.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace ExamenPrimerParcial
 {
-    internal class Alumno
+    public class Alumno
     {
+       public string NombreAlumno { get; set; }
+        public string NumeroCuenta { get; set; }
+        public string Email { get; set; }
+
+
+        publci 
     }
 }
